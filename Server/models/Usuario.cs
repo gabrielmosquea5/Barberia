1544940674e1351f54d8c0233d4f44386b23cc26
gabrielmosquea5 <117235098+gabrielmosquea5@users.noreply.github.com>
2023-01-25 09:53:39 -0400

    first commit

---
+++ Server/models/Usuario.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using BARBERIA.Shared.Requests;
+
+
+namespace BARBERIA.Server.Models;
+
+
+public class Usuario
+
+{
+    [Key]
+    public int Id { get; set; }
+    public int UsuarioRolId { get; set; }
+    public UsuarioRol UsuarioRol { get; set; } = default!;
+    public string Nombre { get; set; } = null!;
+    public string Correo { get; set; } = null!;
+    public string Contrasena { get; set; } = null!;
+    public static Usuario Crear(UsuarioRequest request) 
+    {
+        return new Usuario()
+        {
+            UsuarioRolId = request.UsuarioRolId,
+            Nombre = request.Nombre,
+            Correo = request.Correo,
+            Contrasena = request.Contrasena
+        };
+    }
+}
