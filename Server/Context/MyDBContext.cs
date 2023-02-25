62118516be39649e43b4c17357f1372d24d1c636
zacaria-concepcion <zacaria4bgc@gmail.com>
2023-02-25 03:11:56 +0100

    ..

--- Server/Context/MyDBContext.cs
+++ Server/Context/MyDBContext.cs
@@ -1,34 +1,39 @@
 using Microsoft.EntityFrameworkCore;
 using Barberia.Server.Models;
 
-  Barberia.Server.Context;
+ namespace Barberia.Server.Context;
 
- internal interface IMyDbContext
+public interface IMyDbContext
 {
-    DbSet<Usuario> Usuarios { get; set; }
     DbSet<UsuarioRol> UsuariosRoles { get; set; }
+    DbSet<Usuario> Usuarios { get; set; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
 
-internal class MyDbContext : DbContext, IMyDbContext
+public class MyDbContext : DbContext, IMyDbContext
 {
     //Constructor de la clase 
-    protected readonly IConfiguration _configuration;
-    public MyDbContext(IConfiguration configuration) 
-    { 
-        _configuration = configuration;
+    protected readonly IConfiguration configuration;
+    public MyDbContext(IConfiguration _configuration)
+    {
+        configuration = _configuration;
     }
+
+
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-        options.UseSqlServer(_configuration.GetConnectionString("CRUD"));
+        optionsBuilder.UseSqlServer(config.GetConnectionString("Barberia"));
     }
-    #region Tablas de la BD.
-    public DbSet<Usuario> Usuarios { get; set; } = null!;
-    public DbSet<UsuarioRol> UsuariosRoles { get; set; } = null!;
-    #endregion
+
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         return base.SaveChangesAsync(cancellationToken);
     }
+
+    #region Tablas de mi base de datos
+    public DbSet<UsuarioRol> UsuariosRoles { set; get; } = null!;
+
+    public DbSet<Usuario> Usuarios { set; get; } = null!;
+    #region
 }
--- Server/models/UsuarioRol.cs
+++ Server/models/UsuarioRol.cs
@@ -6,9 +6,11 @@ public class UsuarioRol
 {
     [Key]
     public int Id { get; set; }
-    public string RNombre { get; set; } = null!;
+    public string Nombre { get; set; } = null!;
     public bool PermisoParaCrear { get; set; }
     public bool PermisoParaEditar { get; set; }
-    public bool PermisoParaCEliminar { get; set; }
+    public bool PermisoParaEliminar { get; set; }
     public virtual ICollection<UsuarioRol>? Usuarios { get; set; }
 }
+
+
