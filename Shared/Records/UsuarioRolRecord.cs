25c650d21abf62a5903a198866654b8d7947514f
zacaria-concepcion <zacaria4bgc@gmail.com>
2023-03-18 02:35:01 +0100

    Cambio del proyecto

--- Shared/Records/UsuarioRolRecord.cs
+++ Shared/Records/UsuarioRolRecord.cs
@@ -1,12 +1,24 @@
 
 namespace Barberia.Shared.Records;
 
-public class UsuarrioRolRecord
+public class UsuarioRolRecord
 {
-  public int Id { get; set; }
+    public UsuarioRolRecord()
+    {
+        
+    }
+    public UsuarioRolRecord(int id, string nombre, bool permisoParaCrear, bool permisoParaEditar, bool permisoParaEliminar)
+    {
+        this.Id = id;
+        this.Nombre = nombre;
+        this.PermisoParaCrear = permisoParaCrear;
+        this.PermisoParaEditar = permisoParaEditar;
+        this.PermisoParaEliminar = permisoParaEliminar;
+    }
+
+    public int Id { get; set; }
     public string Nombre { get; set; } = null!;
     public bool PermisoParaCrear { get; set; }
     public bool PermisoParaEditar{ get; set; }
-    public bool PermisoParaEliminar{ get; set; }
-
+    public bool PermisoParaEliminar{ get; set; } = null!;
 }
