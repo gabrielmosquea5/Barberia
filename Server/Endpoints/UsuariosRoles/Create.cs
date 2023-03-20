ff07a1d7721f915b863ad5bd3635e4261dace290
zacaria-concepcion <zacaria4bgc@gmail.com>
2023-03-20 15:02:51 +0100

    ...

---
+++ Server/Endpoints/UsuariosRoles/Create.cs
@@ -0,0 +1,12 @@
+using Barberia.Shared.Records;
+using Barberia.Shared.Wrapper;
+using Barberia.Shared.Requests;
+
+namespace Barberia.Server.Endpoints.UsuariosRoles;
+
+using Request = UsuarioRolRecordCreateRequest;
+
+public class Create
+{
+
+}
